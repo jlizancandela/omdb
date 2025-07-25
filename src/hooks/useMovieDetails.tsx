33d854d1ffa1a3e811d67a059499fcf92849c185
Jorge Lizancandela <jlizancandela@gmail.com>
2025-07-26 01:40:10 +0200

    refactor: Move favorite logic to useMovieDetails hook
    Extract the logic for managing favorite movies from the `Movie` component and encapsulate it within the `useMovieDetails` custom hook.
    
    The hook now exposes `toggleFav` and `isFav` functions, which are used in the `Movie` component to add/remove a movie from favorites and to display the correct button state ("like" or "dislike").
    
    This change centralizes the state management logic, simplifies the `Movie` component, and improves code reusability.

--- src/hooks/useMovieDetails.tsx
+++ src/hooks/useMovieDetails.tsx
@@ -1,11 +1,31 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import type { OmdbMovieDetails } from "../models/omdb";
 import { getMovieById } from "../services/omdb";
+import { Context } from "../context/Provider";
 
 export const useMovieDetails = (id: string) => {
   const [data, setData] = useState<OmdbMovieDetails>();
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const ctx = useContext(Context);
+  if (!ctx) {
+    throw new Error("useFavorites must be used within a Context.Provider");
+  }
+
+  const { fav, setFav } = ctx;
+
+  const toggleFav = (movie: OmdbMovieDetails) => {
+    const exists = fav.some((favMovie) => favMovie.imdbID === movie.imdbID);
+    if (exists) {
+      setFav(fav.filter((favMovie) => favMovie.imdbID !== movie.imdbID));
+    } else {
+      setFav([...fav, movie]);
+    }
+  };
+
+  const isFav = (id: string) => {
+    return fav.some((favMovie) => favMovie.imdbID === id);
+  };
 
   useEffect(() => {
     if (!id) return;
@@ -18,5 +38,5 @@ export const useMovieDetails = (id: string) => {
       .finally(() => setLoading(false));
   }, [id]);
 
-  return { data, error, loading };
+  return { data, error, loading, toggleFav, isFav };
 };
--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,28 +1,10 @@
 import { useParams } from "react-router";
 import { useMovieDetails } from "../hooks/useMovieDetails";
 import styles from "./Movie.module.css";
-import { useContext } from "react";
-import { Context } from "../context/Provider";
-import type { OmdbMovieDetails } from "../models/omdb";
 
 export const Movie = () => {
   const { id } = useParams<{ id: string }>();
-  const { data, loading, error } = useMovieDetails(id || ""); // TODO: Añadir el estado de like
-
-  const ctx = useContext(Context);
-  if (!ctx) return null; // protección si está fuera del Provider
-
-  const { lastPage, fav, setFav, setLastpage } = ctx;
-
-  //TODO: Exportar la función
-  const addFav = (data: OmdbMovieDetails) => {
-    const exists = fav.some((movie) => movie.imdbID === id);
-    if (!exists) {
-      setFav([...fav, data]);
-    } else {
-      setFav(fav.filter((movie) => movie.imdbID !== id));
-    }
-  };
+  const { data, loading, error, isFav, toggleFav } = useMovieDetails(id || "");
 
   if (loading) {
     return <p>Loading...</p>;
@@ -45,8 +27,9 @@ export const Movie = () => {
           <p>Genre: {data?.Genre}</p>
           <p>Runtime: {data?.Runtime}</p>
           <p>IMDB Rating: {data?.imdbRating}</p>
-          <button onClick={() => addFav(data)}>like</button> // TODO: Crear un
-          componente dinamico para el like
+          <button onClick={() => toggleFav(data)}>
+            {isFav(id || "") ? "dislike" : "like"}
+          </button>
         </div>
       </div>
     );
